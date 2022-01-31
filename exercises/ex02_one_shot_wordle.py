cb4620cdab499a5b8943a6846a83b8d50a995510
Jholroyd1 <jackholroyd96@gmail.com>
2022-01-31 12:06:05 -0500

    Exercise 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,45 +1,40 @@
-"""EX02 - One Shot Wordle, another step towards Wordle"""
+"""EX02 - One Shot Wordle, another step towards Wordle."""
 
 
 __author__ = "730322060"
 
 secret: str = str("python")
-guess: str = input("What is your " + str(len(secret)) + " letter guess? ")
-i: int = len(guess)
-
+guess: str = input("What is your " + str(len(secret)) + "-letter guess? ")
+i: int = 0
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
 index: int = 0
-White_box: str = "\U00002B1C"
-Green_box: str = "\U0001F7E9"
-Yellow_box: str = "\U0001F7E8"
-Emoji: str = ""
-character_exists: bool = False
-alternate_indices: int = 0
+emoji: str = str("")
 
-
-while i <= 6:
-    if len(guess) == 6:
+while i <= len(secret):
+    if len(guess) == len(secret):
+        while index < len(secret):
+            if guess[index] == secret[index]:
+                emoji = emoji + GREEN_BOX
+            else:
+                character_exists: bool = False
+                alt_index: int = 0
+                while character_exists is False and alt_index < len(secret):
+                    if guess[index] == secret[alt_index]:
+                        character_exists = True
+                    else:
+                        alt_index = alt_index + 1
+                if character_exists is True:
+                    emoji = emoji + YELLOW_BOX
+                else:
+                    emoji = emoji + WHITE_BOX
+            index = index + 1
+        print(emoji)
         if guess == secret:
             print("Woo! You got it!")
         else:
             print("Not quite. Play again soon!")
-        i = i + 1
+        i = len(secret) + 1
     else:
-        guess: str = input("That was not " + str(len(secret)) + " letters! Try again: ")
-
-
-while index < len(secret):
-    if guess[index] == secret[index]:
-        Emoji = Emoji + Green_box
-    else:
-        while alternate_indices < len(secret):
-            if guess[index] == secret[alternate_indices]:
-                character_exists is True
-            else:
-                alternate_indices = alternate_indices + 1
-        if character_exists is True:
-            Emoji = Emoji + Yellow_box
-        else:
-            Emoji = Emoji + White_box
-    index = index + 1
-
-print(Emoji)
+        guess = input("That was not " + str(len(secret)) + " letters! Try again: ")
